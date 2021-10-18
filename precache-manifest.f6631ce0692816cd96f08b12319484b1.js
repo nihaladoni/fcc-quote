6b806e0fbbc4063da145370482133ba29950b87d
nihal <nihaladoni@gmail.com>
2021-10-18 22:35:07 +0530

    Updates

---
+++ precache-manifest.f6631ce0692816cd96f08b12319484b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44d95c8fccf95f5ad6e2a68c203f27c",
+    "url": "/fcc-quote/index.html"
+  },
+  {
+    "revision": "f9340af0aa5b66d1b401",
+    "url": "/fcc-quote/static/css/2.cbdafa58.chunk.css"
+  },
+  {
+    "revision": "7a3f0c5e5df64833ca63",
+    "url": "/fcc-quote/static/css/main.e12e19f6.chunk.css"
+  },
+  {
+    "revision": "f9340af0aa5b66d1b401",
+    "url": "/fcc-quote/static/js/2.6900afef.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "/fcc-quote/static/js/2.6900afef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3f0c5e5df64833ca63",
+    "url": "/fcc-quote/static/js/main.54745eda.chunk.js"
+  },
+  {
+    "revision": "1d759401ccf9df9c875d",
+    "url": "/fcc-quote/static/js/runtime-main.e972cc8b.js"
+  }
+]);
